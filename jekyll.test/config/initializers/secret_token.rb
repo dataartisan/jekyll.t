91a6b06d4c9758c0395d03e6dd2bd0dbd045ff0f
Monica Sharma <monicasharma1053@gmail.com>
2012-11-15 13:42:22 -0600

    rails j.test

---
+++ jekyll.test/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JekyllTest::Application.config.secret_token = '1f1687d79c5bb39702bc507d6f48aa35897468438a28dfb143b7545340a8512c858db889087f2d5c834956c764ebe87be2c6b512fa07a98fb121210452854187'
